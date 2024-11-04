9d4c850f0a89490d721acf2a11ed9196f0554716
Tyler Russell <82682594+Tyler-251@users.noreply.github.com>
2024-11-04 00:00:07 -0600

    flashy nana

--- src/banana.rs
+++ src/banana.rs
@@ -1,9 +1,13 @@
 use bevy::{animation::transition, prelude::*, render::camera};
+use bevy::core_pipeline::motion_blur::*;
+use bevy::core_pipeline::bloom::*;
+use bevy::core_pipeline::tonemapping::Tonemapping;
 
 pub struct BananaPlugin;
 
 impl Plugin for BananaPlugin { // made a plugin that calls spawn_scene and move_banana
     fn build(&self, app: &mut App) {
+        app.insert_resource(Msaa::Off);
         app.add_systems(Startup, spawn_scene);
         app.add_systems(Update, move_banana);
     }
@@ -17,39 +21,58 @@ pub struct Banana {
 static BANANA_RATIO: f32 = 1470. / 986.; // y to x ratio of the banana image
 pub fn spawn_scene (
     mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
     asset_server: ResMut<AssetServer>,
 ) {
-    commands.spawn(
-        Camera2dBundle::default() //camera spawn
-    );
+    commands.spawn((
+        Camera2dBundle {
+            camera: Camera {
+                hdr: true,
+                ..default()
+            },
+            tonemapping: Tonemapping::AgX,
+            ..default()
+        },
+        MotionBlur {
+            shutter_angle: 500.0,
+            samples: 3,
+        },
+        BloomSettings::default(),
+    ));
 
     let banana: Handle<Image> = asset_server.load("imgs/nana.png"); //loading asset
     let banana_height: f32 = 80.0; 
     commands.spawn( (//sprite spawning
-        Banana{velocity: Vec2::new(0.0, 0.0)},
+        Banana{velocity: Vec2::new(0., 0.)},
         SpriteBundle {
-            texture: banana, //image
+            texture: banana.clone(), //image
             sprite: Sprite { //size
                 custom_size: Some(Vec2::new(banana_height * BANANA_RATIO, banana_height)),
+                color: Color::srgb(5.0, 5.0, 5.0),
                 ..default()
             },
             transform: Transform::from_xyz(0.0, 0.0, 0.0), //position
             ..default()
         },
     ));
+
+    commands.spawn(
+        PbrBundle {
+            mesh: meshes.add(Cuboid::new(1.0, 1.0, 1.0)),
+            transform: Transform::from_xyz(0., 0., -5.),
+            ..default()
+        }
+    );
 }
 
 static BANANA_SPEED: f32 = 150.0;
 pub fn move_banana (
-    mut banana: Query<(&mut Transform, &mut Banana)>,
-    mut sprite: Query<&mut Sprite, With<Banana>>,
+    mut banana: Query<(&mut Transform, &mut Banana, &mut Sprite)>,
     camera_query: Query<(&GlobalTransform, &Camera)>,
     window_query: Query<&Window>,
     input: Res<ButtonInput<KeyCode>>,
     time: Res<Time>,
 ) {
-    let (mut banana_transform, mut banana_struct) = banana.single_mut();
-    let mut banana_sprite = sprite.single_mut();
     let (camera_transform, camera) = camera_query.single();
     let window = window_query.single();
     let mut direction = Vec2::ZERO;
@@ -62,22 +85,23 @@ pub fn move_banana (
         return;
     };
 
-    let mut desired_velocity = destination - banana_transform.translation.truncate();
-    let distance = desired_velocity.length();
+    for (mut banana_transform, mut banana_struct, mut sprite) in banana.iter_mut() {
+        let mut desired_velocity = destination - banana_transform.translation.truncate();
+        let distance = desired_velocity.length();
+        
 
+        if distance > 0. {
+            let mut distance_quotient = 0.05;
 
-    if distance > 0. {
-        let mut distance_quotient = 0.05;
+            desired_velocity = desired_velocity.normalize() * distance * 0.7;
+            banana_struct.velocity = (desired_velocity * distance_quotient) + (banana_struct.velocity * (1. - distance_quotient));
+        }
+        
+        banana_transform.translation.x += banana_struct.velocity.x * time.delta_seconds() * BANANA_SPEED;
+        banana_transform.translation.y += banana_struct.velocity.y * time.delta_seconds() * BANANA_SPEED;
 
-        desired_velocity = desired_velocity.normalize() * distance * 0.7;
-        banana_struct.velocity = (desired_velocity * distance_quotient) + (banana_struct.velocity * (1. - distance_quotient));
+        if distance > 100. {
+            sprite.flip_x = banana_struct.velocity.x < 0.;
+        }
     }
-
-
-
-    banana_transform.translation.x += banana_struct.velocity.x * time.delta_seconds() * BANANA_SPEED;
-    banana_transform.translation.y += banana_struct.velocity.y * time.delta_seconds() * BANANA_SPEED;
-    println!("{}", banana_struct.velocity);
-
-
 }
